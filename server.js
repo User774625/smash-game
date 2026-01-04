2d866cd9807ff439e2316979657d7e1641001944
User774625 <strohm.julius@gmx.de>
2026-01-04 22:03:35 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const http = require('http');
 const socketIo = require('socket.io');
 
 const app = express();
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'dist')));  // Dein Build-Ordner
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: { origin: "*" }
